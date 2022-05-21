898eaf8471aa4db0637c476b9fb44513d2af1980
vuducnguyentb <vuducnguyentb@gmail.com>
2022-05-21 11:27:20 +0700

    update project

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">User Info</div>
+
+                    <div class="card-body">
+                       <p>User Name : {{user.name}}</p>
+                       <p>User Email : {{user.email}}</p>
+                       <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return{
+              user: {}
+          }  
+        },
+        mounted() {
+            console.log('Component mounted.');
+            let _this = this;
+            window.Echo.channel('chat')
+                .listen('NewMessage', (data) => {
+                    console.log('Chạy vào kênh r');
+                console.log('data', data);
+                _this.user = data.user;
+                        });
+        }
+    }
+</script>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Events\NewMessage;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('test-event',function(){
+        broadcast(new NewMessage(auth()->user()));
+        echo 'ok men!';
+    });
+});
